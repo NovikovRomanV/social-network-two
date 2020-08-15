637d7f98c9c1a42ec8224bcfe4081ca8b1f7454a
NovikovRomanV <rimm19845@gmail.com>
2020-08-15 14:10:47 +0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img alt='fon'
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSoI82dQ4RRe2jJboyhw2sLZofK-Kt8GhMQMg&usqp=CAU'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
